ed1d1db84507cf9e3cc96aee4649a798b069442d
Ryszawy <Krafi604@gmail.com>
2026-02-17 22:45:40 +0100

    Head A: refactor 2

--- mmsegmentation/mmseg/models/decode_heads/segformer_conv_head.py
+++ mmsegmentation/mmseg/models/decode_heads/segformer_conv_head.py
@@ -1,4 +1,5 @@
 import torch
+import torch.nn as nn
 from mmcv.cnn import ConvModule
 from mmseg.registry import MODELS
 from mmseg.models.utils import resize
@@ -7,21 +8,25 @@ from .segformer_head import SegformerHead
 
 @MODELS.register_module()
 class SegFormerConvHead(SegformerHead):
-    """SegFormer head with conv-based fusion replacing fusion_conv."""
+    """SegFormer head with conv-based fusion replacing fusion_conv.
+    Uses LazyConv2d to infer concat channel dim at runtime (version-robust).
+    """
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
-        # W SegFormerHead: fusion_conv przyjmuje concat z 4 skal:
-        # in_channels = sum(self.in_channels) (czyli np. 32+64+160+256=512 dla B0)
-        in_ch = sum(self.in_channels)
-        out_ch = self.channels  # embed dim (np. 256)
+        out_ch = self.channels  # embed dim used by cls_seg
 
-        # Nasz blok fuse: 1x1 -> DW 3x3 -> 1x1
-        self.fuse_conv1 = ConvModule(
-            in_ch, out_ch, kernel_size=1,
-            norm_cfg=self.norm_cfg, act_cfg=dict(type='ReLU')
-        )
+        # 1x1 (lazy in_channels) -> DW 3x3 -> 1x1
+        self.fuse_conv1 = nn.LazyConv2d(out_ch, kernel_size=1, bias=False)
+
+        # norm/act around lazy conv (keep simple & robust)
+        # Use the same norm type as in the base head via ConvModule for later blocks.
+        # For the first block we add BN+ReLU explicitly.
+        # NOTE: If you use SyncBN on 1 GPU, it still works but BN is fine too.
+        self.fuse_bn1 = nn.BatchNorm2d(out_ch)
+        self.fuse_act1 = nn.ReLU(inplace=True)
+
         self.fuse_dwconv = ConvModule(
             out_ch, out_ch, kernel_size=3, padding=1, groups=out_ch,
             norm_cfg=self.norm_cfg, act_cfg=dict(type='ReLU')
@@ -32,7 +37,6 @@ class SegFormerConvHead(SegformerHead):
         )
 
     def forward(self, inputs):
-        # 1) dokładnie jak w SegformerHead.forward()
         inputs = self._transform_inputs(inputs)
         outs = []
         for idx in range(len(inputs)):
@@ -43,15 +47,19 @@ class SegFormerConvHead(SegformerHead):
                     input=conv(x),
                     size=inputs[0].shape[2:],
                     mode=self.interpolate_mode,
-                    align_corners=self.align_corners))
+                    align_corners=self.align_corners
+                )
+            )
 
         x = torch.cat(outs, dim=1)
 
-        # 2) nasz fuse zamiast self.fusion_conv(...)
+        # Lazy 1x1 + BN + ReLU
         x = self.fuse_conv1(x)
+        x = self.fuse_bn1(x)
+        x = self.fuse_act1(x)
+
         x = self.fuse_dwconv(x)
         x = self.fuse_conv2(x)
 
-        # 3) klasyfikator jak w bazie
         x = self.cls_seg(x)
         return x
