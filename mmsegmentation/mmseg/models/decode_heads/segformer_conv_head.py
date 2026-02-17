161b272ab5b1a106e6df70aa6be20a9a11f10a58
Ryszawy <Krafi604@gmail.com>
2026-02-17 22:38:21 +0100

    Head A: refactor 1

--- mmsegmentation/mmseg/models/decode_heads/segformer_conv_head.py
+++ mmsegmentation/mmseg/models/decode_heads/segformer_conv_head.py
@@ -1,54 +1,57 @@
 import torch
-import torch.nn as nn
 from mmcv.cnn import ConvModule
 from mmseg.registry import MODELS
+from mmseg.models.utils import resize
 from .segformer_head import SegformerHead
 
 
 @MODELS.register_module()
 class SegFormerConvHead(SegformerHead):
+    """SegFormer head with conv-based fusion replacing fusion_conv."""
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
-        embed_dim = self.channels
+        # W SegFormerHead: fusion_conv przyjmuje concat z 4 skal:
+        # in_channels = sum(self.in_channels) (czyli np. 32+64+160+256=512 dla B0)
+        in_ch = sum(self.in_channels)
+        out_ch = self.channels  # embed dim (np. 256)
 
-        # zastępujemy linearny fuse MLP
+        # Nasz blok fuse: 1x1 -> DW 3x3 -> 1x1
         self.fuse_conv1 = ConvModule(
-            embed_dim,
-            embed_dim,
-            kernel_size=1,
-            norm_cfg=self.norm_cfg,
-            act_cfg=dict(type='ReLU')
+            in_ch, out_ch, kernel_size=1,
+            norm_cfg=self.norm_cfg, act_cfg=dict(type='ReLU')
         )
-
         self.fuse_dwconv = ConvModule(
-            embed_dim,
-            embed_dim,
-            kernel_size=3,
-            padding=1,
-            groups=embed_dim,
-            norm_cfg=self.norm_cfg,
-            act_cfg=dict(type='ReLU')
+            out_ch, out_ch, kernel_size=3, padding=1, groups=out_ch,
+            norm_cfg=self.norm_cfg, act_cfg=dict(type='ReLU')
         )
-
         self.fuse_conv2 = ConvModule(
-            embed_dim,
-            embed_dim,
-            kernel_size=1,
-            norm_cfg=self.norm_cfg,
-            act_cfg=dict(type='ReLU')
+            out_ch, out_ch, kernel_size=1,
+            norm_cfg=self.norm_cfg, act_cfg=dict(type='ReLU')
         )
 
     def forward(self, inputs):
-        # to jest oryginalny flow SegformerHead
-        x = self._forward_feature(inputs)
+        # 1) dokładnie jak w SegformerHead.forward()
+        inputs = self._transform_inputs(inputs)
+        outs = []
+        for idx in range(len(inputs)):
+            x = inputs[idx]
+            conv = self.convs[idx]
+            outs.append(
+                resize(
+                    input=conv(x),
+                    size=inputs[0].shape[2:],
+                    mode=self.interpolate_mode,
+                    align_corners=self.align_corners))
 
-        # NASZ NOWY FUSE BLOK
+        x = torch.cat(outs, dim=1)
+
+        # 2) nasz fuse zamiast self.fusion_conv(...)
         x = self.fuse_conv1(x)
         x = self.fuse_dwconv(x)
         x = self.fuse_conv2(x)
 
+        # 3) klasyfikator jak w bazie
         x = self.cls_seg(x)
-
         return x
